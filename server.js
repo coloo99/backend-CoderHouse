e216e878435980e7093733982c608d05697c965e
Lucas Yacusa <lyacusa99@gmail.com>
2022-10-12 20:47:29 -0300

    Desaffio 5 con handlebars terminado

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const handlebars = require("express-handlebars")
 const productosRouter = require("./routes/productos")
 const app = express()
+
+//Contenedor
+const co = require("./script")
+const conte = new co("productos.txt")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -8,3 +13,32 @@ app.use(express.urlencoded({ extended: true }))
 app.listen(8080, ()=>console.log("server is listening on port 8080"))
 
 app.use("/productos", productosRouter)
+
+//Defino el motor de plantillas
+app.engine("handlebars",handlebars.engine())
+
+//Ubico la carpeta o directorio donde ubico los templates .handlebars
+app.set("views", "./views")
+
+//Defino el motor para express
+app.set("view engine", "handlebars")
+
+//Ruta para mostrar los productos en una tabla
+app.get("/mostrarProductos", async (request, response)=>{
+    const productos = await conte.getAll();
+    response.render("mostrarProductos", {productos})
+})
+
+//Ruta con formulario para agregar los productos mediante post
+app.get("/agregarProductos", async (request, response)=>{
+    const productos = await conte.getAll();
+    response.render("agregarProductos", {productos})
+})
+
+//Post de los productos
+app.post("/", async (request, response)=>{
+    const newProducto = request.body
+    console.log(newProducto)
+    const productoAgregado = await conte.save(newProducto)
+    response.redirect("/mostrarProductos")
+})
