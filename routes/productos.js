cd05171519d22430a12bd4569177d39e963caa3b
Lucas Yacusa <lyacusa99@gmail.com>
2022-10-14 15:42:45 -0300

    Desaffio con pug terminado

--- routes/productos.js
+++ routes/productos.js
@@ -5,13 +5,13 @@ const co = require("../script")
 const conte = new co("productos.txt")
 
 //Obtengo todos los productos
-router.get("/", async (request, response)=>{
+/* router.get("/", async (request, response)=>{
     const productos = await conte.getAll();
     response.send(productos);
-})
+}) */
 
 //Obtengo el producto segun la id 
-router.get("/:productoId", async (request, response)=>{
+/* router.get("/:productoId", async (request, response)=>{
     const productos = await conte.getAll();
     const {productoId} = request.params;
     const producto = productos.find(elem=>elem.id === parseInt(productoId));
@@ -20,13 +20,7 @@ router.get("/:productoId", async (request, response)=>{
     }else{
         response.send(producto);
     }
-})
-
-router.post("/", async (request, response)=>{
-    const newProducto = request.body
-    const productoAgregado = await conte.save(newProducto)
-    response.json({msg:"Post realizado correctamente", data: productoAgregado})
-})
+}) */
 
 router.put("/:id", async (request, response)=>{
     const {id} = request.params;
@@ -51,6 +45,26 @@ router.delete("/:id", async (request, response)=>{
     const {id} = request.params
     await conte.deleteById(parseInt(id))
     response.send("Peroducto eliminado con exito")
+})
+
+//Ruta para mostrar los productos en una tabla
+router.get("/mostrarProductos", async (request, response)=>{
+    const productos = await conte.getAll();
+    response.render("mostrarProductos", {productos})
+})
+
+//Ruta con formulario para agregar los productos mediante post
+router.get("/agregarProductos", async (request, response)=>{
+    const productos = await conte.getAll();
+    response.render("agregarProductos", {productos})
+})
+
+//Post de los productos
+router.post("/", async (request, response)=>{
+    const newProducto = request.body
+    console.log(newProducto)
+    const productoAgregado = await conte.save(newProducto)
+    response.redirect("/productos/mostrarProductos")
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -14,31 +14,8 @@ app.listen(8080, ()=>console.log("server is listening on port 8080"))
 
 app.use("/productos", productosRouter)
 
-//Defino el motor de plantillas
-app.engine("handlebars",handlebars.engine())
-
 //Ubico la carpeta o directorio donde ubico los templates .handlebars
 app.set("views", "./views")
 
 //Defino el motor para express
-app.set("view engine", "handlebars")
-
-//Ruta para mostrar los productos en una tabla
-app.get("/mostrarProductos", async (request, response)=>{
-    const productos = await conte.getAll();
-    response.render("mostrarProductos", {productos})
-})
-
-//Ruta con formulario para agregar los productos mediante post
-app.get("/agregarProductos", async (request, response)=>{
-    const productos = await conte.getAll();
-    response.render("agregarProductos", {productos})
-})
-
-//Post de los productos
-app.post("/", async (request, response)=>{
-    const newProducto = request.body
-    console.log(newProducto)
-    const productoAgregado = await conte.save(newProducto)
-    response.redirect("/mostrarProductos")
-})
+app.set("view engine", "pug")
